d276b55bc3fe5a45cd61bf09308228d04c692f84
Gregorio López <gregoriolopeez@gmail.com>
2026-03-31 12:28:24 +0200

    resolucion acabada con su test

--- src/TestResolucion.java
+++ src/TestResolucion.java
@@ -1,5 +1,22 @@
+import java.awt.*;
+
 public class TestResolucion {
     public static void main(String[] args) {
+        Dimension[] dimensiones = {
+                new Dimension(4096,2160),
+                new Dimension(1920,1080),
+                new Dimension(2560,1440),
+                new Dimension(800,600)
 
+        };
+        Resolucion[] resoluciones = {
+                Resolucion.VENTANA_4K,
+                Resolucion.VENTANA_FULLHD,
+                Resolucion.VENTANA_QHD,
+                Resolucion.VENTANA_VGA
+        };
+        for (int i = 0; i < resoluciones.length; i++) {
+            System.out.println(resoluciones[i].getResolucion().equals(dimensiones[i]));
+        }
     }
 }
