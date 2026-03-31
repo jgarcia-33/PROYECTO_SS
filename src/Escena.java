b10bfaff24dc15873f31bbdfe0d7487311cf4112
roberto <maritocanta3@gmail.com>
2026-03-31 12:40:11 +0200

    Escena y juego

--- src/Escena.java
+++ src/Escena.java
@@ -1,5 +1,6 @@
 import bpc.daw.consola.Consola;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Escena implements ElementoJuego{
@@ -15,6 +16,22 @@ public abstract class Escena implements ElementoJuego{
 
     }
     public void retirar(GameObject obj){
+
+    }
+    @Override
+    public void inicializar(){
+        if (this.consola == null || this.juego == null){
+            throw new IllegalStateException("Hay que añadir la escena al juego para poder inicializarla");
+        }
+        this.objetos = new ArrayList<>();
+        this.añadirObjetosIniciales();
+    }
+    @Override
+    public void ejecutarFrame(){
+
+    }
+    @Override
+    public void finalizar(){
 
     }
 }
--- src/Juego.java
+++ src/Juego.java
@@ -6,7 +6,10 @@ public class Juego {
     private boolean detener;
     private Resolucion resolucion;
 
-    public void iniciar(Escena e,Resolucion r){}
+    public void iniciar(Escena e,Resolucion r){
+        this.resolucion = r;
+        this.consola = new Consola();
+    }
     public Escena getEscena(){
         return this.escena;
     }
@@ -17,6 +20,9 @@ public class Juego {
         this.detener = true;
     }
     public int getAnchuraPantalla(){
-        return
+        return this.resolucion.getDimension().width;
+    }
+    public int getAlturaPantalla(){
+        return this.resolucion.getDimension().width;
     }
 }
