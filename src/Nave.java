3de2dfd93509dea7b55c028da3ae4d44cb78e0b9
javiercaslop0310 <javiercaslop0310@gmail.com>
2026-04-04 19:36:13 +0200

    Add Disparo and all Nave

---
+++ src/Nave.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.time.Instant;
+import bpc.daw.consola.*;
+
+
+public abstract class Nave extends SpriteGameObject {
+    private int teclaIzquierda;
+    private int teclaDerecha;
+    private int teclaDisparo;
+    private int velocidad;
+    private Instant ultimoDisparoRealizado;
+
+    public Nave (int ti, int tde, int tdi, int v, Image image){
+        super(image,image.getWidth(null),image.getHeight(null));
+        this.teclaIzquierda=ti;
+        this.teclaDerecha=tde;
+        this.teclaDisparo=tdi;
+        this.velocidad=v;
+        this.ultimoDisparoRealizado=Instant.now().minusSeconds(1);
+    }
+
+    @Override
+    public void ejecutarFrame() {
+        if (consola.getTeclado().teclaPulsada(this.teclaIzquierda)){
+            this.moverX(this.velocidad*-1);
+        }
+        if (consola.getTeclado().teclaPulsada(this.teclaDerecha)){
+            this.moverX(this.velocidad);
+        }
+        if (consola.getTeclado().teclaPulsada(this.teclaDisparo)){
+            this.disparar();
+        }
+    }
+
+    public void disparar(){
+        if (Instant.now().minusSeconds(1).isAfter(this.ultimoDisparoRealizado)){
+            this.ultimoDisparoRealizado=Instant.now();
+            this.realizarDisparo();
+        }
+    }
+    protected abstract void realizarDisparo();
+}
